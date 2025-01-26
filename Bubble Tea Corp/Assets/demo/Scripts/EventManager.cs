55012283f6dea9765c2ca0f93c6127c69a4c03e3
MMPrins1000 <michel.m.prins@gmail.com>
2025-01-26 14:51:53 +0100

    up
    as if we have time to write or read this

--- Bubble Tea Corp/Assets/demo/Scripts/EventManager.cs
+++ Bubble Tea Corp/Assets/demo/Scripts/EventManager.cs
@@ -118,6 +118,7 @@ public class EventManager : MonoBehaviour
         //eventData.FIllList();
         //events = eventData.events;
         //choices = eventData.choices;
+        CloseChoicePanel();
     }
 
     void Update()
